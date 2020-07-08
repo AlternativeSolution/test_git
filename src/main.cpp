bf1544269965f763b4a30f341d03653e2d35b02d
AlternativeSolution <wildwolf482@gmail.com>
2020-07-08 11:25:07 +0300

    add large feature in dev branch

--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,5 @@ int main(){
   cout << "I am project for git test";
   cout << "this is new cool feature";
   cout << "someone add new func in master branch";
+  cout << "this is super-mega large new feature from dev branch"; 
 }
